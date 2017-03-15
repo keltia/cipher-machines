f514b91961d41bbd64f31306f486a0712b8b487c
Ollivier Robert <roberto@keltia.net>
2017-03-15 23:44:11 +0100

    Placeholders.

--- enigma_test.go
+++ enigma_test.go
@@ -72,3 +72,15 @@ func TestEnigma_AddReflector(t *testing.T) {
     arrayPfA := []int{24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19}
     assert.EqualValues(t, e.Reflector.rotor, arrayPfA, "should be equal")
 }
+
+func TestEnigma_Encrypt(t *testing.T) {
+
+}
+
+func TestEnigma_Decrypt(t *testing.T) {
+
+}
+
+func TestEnigma_DumpState(t *testing.T) {
+
+}
